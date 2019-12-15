b3ac159342825b3b61e8c936c66f3f7df4ef9bb6
Vadim Kim <17victork@gmail.com>
2019-12-14 22:50:29 -0800

    added mr page

---
+++ Bruh_Mr_Page/sketch.js
@@ -0,0 +1,12 @@
+image
+
+function preload(){
+
+}
+
+function setup() {
+  let myCanvas = createCanvas(innerWidth, innerHeight);
+  myCanvas.parent("mainSketch");
+  background(0);
+  let middleOfCanvas = (innerWidth/2) - ()
+}
