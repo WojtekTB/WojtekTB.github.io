b76f88db57ad3ff6bf6c103423bda3a7ddfb3bbc
Vadim Kim <17victork@gmail.com>
2019-12-29 23:27:30 -0800

    added convey

---
+++ projects/convey/js/Cells.js
@@ -0,0 +1,98 @@
+class Cell_Empty {
+  show() {
+    return;
+  }
+  item_action(item) {
+    return { x: item.x, y: item.y };
+  }
+}
+
+class Cell_Conveyor {
+  constructor(direction, speed) {
+    //direction of the conveyor belt that would be on the cell if any at all
+    this.direction_right = false;
+    this.direction_up = false;
+    this.direction_left = false;
+    this.direction_down = false;
+    if (direction == RIGHT) {
+      this.direction_right = true;
+    } else if (direction == UP) {
+      this.direction_up = true;
+    } else if (direction == LEFT) {
+      this.direction_left = true;
+    } else if (direction == DOWN) {
+      this.direction_down = true;
+    }
+    this.animationFrames = animationFrames_conveyor;
+    this.innerFrameCounter = 0;
+    this.maxFrames = animationFrames_conveyor.length - 1;
+    this.speed = speed;
+  }
+  show(x, y, scale) {
+    /*
+     *if there is a conveyor placed here then draw something
+     * otherwise just skip it all together, no need to render it
+     */
+    translate(x, y);
+    if (this.direction_up) {
+      image(
+        this.animationFrames[Math.floor((frameCount * 0.2) % this.maxFrames)],
+        0,
+        0,
+        scale,
+        scale
+      );
+    } else if (this.direction_right) {
+      rotate(PI / 2);
+      image(
+        this.animationFrames[Math.floor((frameCount * 0.2) % this.maxFrames)],
+        0,
+        -scale,
+        scale,
+        scale
+      );
+      rotate(-PI / 2);
+    } else if (this.direction_left) {
+      rotate((PI * 3) / 2);
+      image(
+        this.animationFrames[Math.floor((frameCount * 0.2) % this.maxFrames)],
+        -scale,
+        0,
+        scale,
+        scale
+      );
+      rotate(-(PI * 3) / 2);
+    } else if (this.direction_down) {
+      rotate(PI);
+      image(
+        this.animationFrames[Math.floor((frameCount * 0.2) % this.maxFrames)],
+        -scale,
+        -scale,
+        scale,
+        scale
+      );
+      rotate(PI);
+    }
+    this.innerFrameCounter += this.speed;
+    if (this.innerFrameCounter > this.maxFrames) {
+      this.innerFrameCounter = 0;
+    }
+    translate(-x, -y);
+  }
+
+  item_action(item) {
+    if (this.direction_right) {
+      //   item.vx += this.speed * 5;
+      item.vx += this.speed * (5 * random(0.2, 1));
+    } else if (this.direction_up) {
+      //   item.vy -= this.speed * 5;
+      item.vy -= this.speed * (5 * random(0.2, 1));
+    } else if (this.direction_left) {
+      //   item.vx -= this.speed * 5;
+      item.vx -= this.speed * (5 * random(0.2, 1));
+    } else if (this.direction_down) {
+      //   item.vy += this.speed * 5;
+      item.vy += this.speed * (5 * random(0.2, 1));
+    }
+  }
+}
---
+++ projects/convey/js/Items.js
@@ -0,0 +1,32 @@
+class Item_Test {
+  constructor(x, y, grid) {
+    this.x = x;
+    this.y = y;
+    this.vx = 0;
+    this.vy = 0;
+    this.friction = 0.85;
+    this.grid = grid;
+  }
+
+  show() {
+    // fill(255, 0, 0);
+    let width = 20;
+    image(
+      test_item_image,
+      this.x - width / 2 + mapOffsetX,
+      this.y - width / 2 + mapOffsetY,
+      width,
+      width
+    );
+    // rect(this.x, this.y, 10, 10);
+  }
+  run() {
+    let gridX = Math.floor(this.x / this.grid.scale);
+    let gridY = Math.floor(this.y / this.grid.scale);
+    this.grid.grid[gridY][gridX].item_action(this);
+    this.x += this.vx;
+    this.y += this.vy;
+    this.vx *= this.friction;
+    this.vy *= this.friction;
+  }
+}
